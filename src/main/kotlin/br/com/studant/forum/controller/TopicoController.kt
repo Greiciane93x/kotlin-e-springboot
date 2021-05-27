8ead3a89ac4461500c3ceced0943d304d37145bb
Ane <greiciane.assis@gclick.com.br>
2021-05-27 18:11:22 -0300

    mais algumas alteracoes

--- src/main/kotlin/br/com/studant/forum/controller/TopicoController.kt
+++ src/main/kotlin/br/com/studant/forum/controller/TopicoController.kt
@@ -1,32 +1,37 @@
 package br.com.studant.forum.controller
 
 
+import br.com.studant.forum.dto.AtualizacaoTopicoForm
 import br.com.studant.forum.dto.NovoTopicoForm
 import br.com.studant.forum.dto.TopicoView
-import br.com.studant.forum.model.Curso
 import br.com.studant.forum.model.Topico
-import br.com.studant.forum.model.Usuario
 import br.com.studant.forum.service.TopicoService
+import org.apache.coyote.Response
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.util.UriComponentsBuilder
 import javax.validation.Valid
 
 @RestController
-@RequestMapping(value = ["/topicos"])
+@RequestMapping("/topicos")
+
 class TopicoController(private val service: TopicoService) {
 
     @GetMapping
-    fun listar(): List<TopicoView> {
-      return service.listar()
-    }
-
+    fun listar(): List<TopicoView> { return service.listar() }
     @GetMapping("/{id}")
-    fun buscarPorId(@PathVariable id: Long): TopicoView {
-        return service.buscarPorId(id)
-    }
-
+    fun buscarPorId(@PathVariable id: Long): TopicoView { return service.buscarPorId(id) }
     @PostMapping
-    fun cadastrar(@RequestBody @Valid dto: NovoTopicoForm){
-        service.cadastrar(dto)
-    }
+    fun cadastrar(
+        @RequestBody @Valid form: NovoTopicoForm,
+        uriBuilder: UriComponentsBuilder
+    ): ResponseEntity<TopicoView> {
+        val topicoView = service.cadastrar(form)
+        val uri = uriBuilder.path("/topicos/${topicoView.id}").build().toUri()
+        return ResponseEntity.created(uri).body(topicoView) }
+    @PutMapping
+    fun atualizar(@RequestBody @Valid form: AtualizacaoTopicoForm){ service.atualizar(form) }
+    @DeleteMapping("/{id}")
+    fun deletar(@PathVariable id: Long){ service.deletar(id) }
 
 }
--- src/main/kotlin/br/com/studant/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/studant/forum/service/TopicoService.kt
@@ -1,6 +1,7 @@
 package br.com.studant.forum.service
 
 
+import br.com.studant.forum.dto.AtualizacaoTopicoForm
 import br.com.studant.forum.dto.NovoTopicoForm
 import br.com.studant.forum.dto.TopicoView
 import br.com.studant.forum.mapper.TopicoFormMapper
@@ -82,9 +83,33 @@ class TopicoService (private var topicos: List<Topico> = ArrayList(),
         return topicoViewMapper.map(topico)
     }
 
-    fun cadastrar(form: NovoTopicoForm){
+    fun cadastrar(form: NovoTopicoForm) : TopicoView{
         val topico = topicoFormMapper.map(form)
         topico.id = topicos.size.toLong()+1
         topicos = topicos.plus(topico)
+        return topicoViewMapper.map(topico)
+    }
+
+    fun atualizar(form: AtualizacaoTopicoForm) {
+        val topico = topicos.stream().filter{ t->
+            t.id == form.id
+        }.findFirst().get()
+        topicos = topicos.minus(topico).plus(Topico(
+          id = form.id,
+          titulo = form.titulo,
+          mensagem = form.mensagem,
+          autor = topico.autor,
+          curso = topico.curso,
+          respostas = topico.respostas,
+            status = topico.status,
+            dataCriacao =  topico.dataCriacao
+        ))
+    }
+
+    fun deletar(id: Long) {
+        val topico = topicos.stream().filter{ t ->
+            t.id == id
+        }.findFirst().get()
+        topicos = topicos.minus(topico)
     }
 }
